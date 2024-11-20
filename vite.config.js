712b1441a6bd0ac2b657cfa5d906c83ae0aabee6
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2024-11-20 03:52:56 +0300

    push dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 import {defineConfig} from "vite";
+import { resolve } from 'path'
 
 export default defineConfig(() => {
     return {
-        build: { outDir: './' },
+        build: {
+            outDir: './dist',
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    cases: resolve(__dirname, 'cases.html')
+                }
+            }
+        },
         // needed for github pages just put the repo name here
         base: 'pasha-cv',
         plugins: [
